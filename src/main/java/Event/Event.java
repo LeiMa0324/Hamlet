46d87d9fe47c1a4703899ae131141b01fd54c2c9
gorillazhip@126.com <ml1989324>
2020-02-21 19:31:13 -0500

    Demo with length of 2 finished

--- src/main/java/Event/Event.java
+++ src/main/java/Event/Event.java
@@ -8,12 +8,11 @@ public class Event {
     private int id; //id in the graphlet
     private int sec;
     private String eventString;
-    //该event的event type,由template查找后返回给event
     private EventType eventType;
 
     /**
-     * 接受一行record，将其转化为event类型
-     * @param line 一行数据
+     * take a line of record，convert it to a Event
+     * @param line line of record
      */
     public Event(String line){
         String[] record = line.split(",");
